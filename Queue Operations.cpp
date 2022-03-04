c03a243cb4d8df3d8c82c423865e32e8ac07f510
Clamendeena.S <79325932+Clamendeena@users.noreply.github.com>
2022-03-04 21:02:12 +0530

    Update Queue Operations.cpp

--- Queue Operations.cpp
+++ Queue Operations.cpp
@@ -1,58 +1,135 @@
+/*
+Queue Operations using templates
+*/
 #include <iostream>
+#include <cstdlib>
 using namespace std;
-int queue[100], n = 100, front = - 1, rear = - 1;
-void Insert() {
-   int val;
-   if (rear == n - 1)
-   cout<<"Queue Overflow"<<endl;
-   else {
-      if (front == - 1)
-      front = 0;
-      cout<<"Insert the element in queue : "<<endl;
-      cin>>val;
-      rear++;
-      queue[rear] = val;
-   }
+ 
+// Define the default capacity of a stack
+#define SIZE 10
+ 
+// A class to represent a stack
+template <class X>
+class stack
+{
+    X *arr;
+    int top;
+    int capacity;
+ 
+public:
+    stack(int size = SIZE);         // constructor
+ 
+    void push(X);
+    X pop();
+    X peek();
+ 
+    int size();
+    bool isEmpty();
+    bool isFull();
+ 
+    // destructor
+    ~stack() {
+        delete[] arr;
+    }
+};
+ 
+// Constructor to initialize the stack
+template <class X>
+stack<X>::stack(int size)
+{
+    arr = new X[size];
+    capacity = size;
+    top = -1;
 }
-void Delete() {
-   if (front == - 1 || front > rear) {
-      cout<<"Queue Underflow ";
-      return ;
-   } else {
-      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
-      front++;;
-   }
+ 
+// Function to add an element `x` to the stack
+template <class X>
+void stack<X>::push(X x)
+{
+    if (isFull())
+    {
+        cout << "Overflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+ 
+    cout << "Inserting " << x << endl;
+    arr[++top] = x;
 }
-void Display() {
-   if (front == - 1)
-   cout<<"Queue is empty"<<endl;
-   else {
-      cout<<"Queue elements are : ";
-      for (int i = front; i <= rear; i++)
-      cout<<queue[i]<<" ";
-         cout<<endl;
-   }
+ 
+// Function to pop the top element from the stack
+template <class X>
+X stack<X>::pop()
+{
+    // check for stack underflow
+    if (isEmpty())
+    {
+        cout << "Underflow\nProgram Terminated\n";
+        exit(EXIT_FAILURE);
+    }
+ 
+    cout << "Removing " << peek() << endl;
+ 
+    // decrease stack size by 1 and (optionally) return the popped element
+    return arr[top--];
 }
-int main() {
-   int ch;
-   cout<<"1) Insert element to queue"<<endl;
-   cout<<"2) Delete element from queue"<<endl;
-   cout<<"3) Display all the elements of queue"<<endl;
-   cout<<"4) Exit"<<endl;
-   do {
-      cout<<"Enter your choice : "<<endl;
-      cin>>ch;
-      switch (ch) {
-         case 1: Insert();
-         break;
-         case 2: Delete();
-         break;
-         case 3: Display();
-         break;
-         case 4: cout<<"Exit"<<endl;
-         break;
-         default: cout<<"Invalid choice"<<endl;
-      }
-   } while(ch!=4);
-   return 0;
+ 
+// Function to return the top element of the stack
+template <class X>
+X stack<X>::peek()
+{
+    if (!isEmpty()) {
+        return arr[top];
+    }
+    else {
+        exit(EXIT_FAILURE);
+    }
+}
+ 
+// Utility function to return the size of the stack
+template <class X>
+int stack<X>::size() {
+    return top + 1;
+}
+ 
+// Utility function to check if the stack is empty or not
+template <class X>
+bool stack<X>::isEmpty() {
+    return top == -1;               // or return size() == 0;
+}
+ 
+// Utility function to check if the stack is full or not
+template <class X>
+bool stack<X>::isFull() {
+    return top == capacity - 1;     // or return size() == capacity;
+}
+ 
+int main()
+{
+    stack<string> pt(2);
+ 
+    pt.push("A");
+    pt.push("B");
+ 
+    pt.pop();
+    pt.pop();
+ 
+    pt.push("C");
+ 
+    // Prints the top of the stack
+    cout << "The top element is " << pt.peek() << endl;
+ 
+    // Returns the total number of elements present in the stack
+    cout << "The stack size is " << pt.size() << endl;
+ 
+    pt.pop();
+ 
+    // check if the stack is empty or not
+    if (pt.isEmpty()) {
+        cout << "The stack is empty\n";
+    }
+    else {
+        cout << "The stack is not empty\n";
+    }
+ 
+    return 0;
 }
